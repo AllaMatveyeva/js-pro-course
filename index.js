759281122a67f1d4151113490dfb3bb6dd435254
Alla Matveyeva <alla.matveyeva.job@gmail.com>
2021-11-07 23:47:36 +0300

    fix task 1 and 3

--- index.js
+++ index.js
@@ -2,54 +2,16 @@ function getVowels(string) {
   const mySet = new Set();
   const stringArr = string.toLowerCase().split("");
   const vowels = ["a", "e", "i", "o", "u", "y"];
-  const arr = [];
+
   for (let str of stringArr) {
     if (vowels.includes(str)) {
       mySet.add(str);
-    } else {
-      continue;
     }
   }
-  mySet.forEach((value) => {
-    arr.push(value);
-  });
-  return arr;
+  return [...mySet];
 }
 const string = "AaBaaaai";
 console.log(getVowels(string));
-
-//////////////////////////////////////
-
-function searchAnnagr(arr) {
-  let arrSort = [];
-  let Annagr = [];
-  let arrInput = [];
-  for (let word of arr) {
-    let sorted = word.toLowerCase().split("").sort().join("");
-    arrSort.push(sorted);
-  }
-
-  for (let i = 0; i < arrSort.length; i++) {
-    if (Annagr.length > 0) {
-      arrInput.push(Annagr);
-      Annagr = [];
-    }
-    for (let j = i; j < arrSort.length; j++) {
-      if (arrSort[i] === arrSort[j]) {
-        if (arr[j] !== 0) {
-          Annagr.push(arr[j]);
-        }
-        arr[j] = 0;
-      } else {
-        continue;
-      }
-    }
-  }
-  console.log(arrInput);
-  return arrInput;
-}
-let arr = ["tom", "xyz", "mot", "xel", "zXy", "yxz"];
-searchAnnagr(arr);
 
 ///////////////////////////////////////
 
@@ -59,21 +21,16 @@ const messages = [
   { text: "See you soon", from: "Alice" },
 ];
 
+let messagesDate = new Map();
+
 function addMessage(message) {
-  let map = new Map();
   let date = new Date();
-  map.set(message, date);
-  let mesInd = messages.indexOf(message);
-  messages[mesInd] = map;
-  return messages[mesInd];
+  messagesDate.set(message, date);
+  return messagesDate;
 }
 
 function getTimeOfMessage(message) {
-  let dateArr = [];
-  for (let mess of message.values()) {
-    dateArr.push(mess);
-  }
-  return dateArr;
+  return messagesDate.get(message);
 }
 
 addMessage(messages[0]);
